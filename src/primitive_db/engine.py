11d7a3f93d6bd9d0165bb655f64a186e1a4ebf9b
Artem Petekhov <apetekhov@gmail.com>
2025-10-15 03:13:37 +0400

    Добавлен линтер Ruff и исправлены ошибки форматирования

--- src/primitive_db/engine.py
+++ src/primitive_db/engine.py
@@ -1,5 +1,6 @@
 import prompt
 
+
 def welcome():
     """
     Приветственная функция.
--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from src.primitive_db.engine import welcome
 
+
 def main():
     welcome()
 
