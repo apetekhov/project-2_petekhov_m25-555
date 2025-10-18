dd8fe6c5d932af15578a26bedbe6bf9e73e799a2
Artem Petekhov <apetekhov@gmail.com>
2025-10-18 17:04:11 +0400

    Обновил print_help с командами управления таблицами

--- src/primitive_db/engine.py
+++ src/primitive_db/engine.py
@@ -30,18 +30,25 @@ from src.primitive_db.utils import (
 
 def print_help() -> None:
     """Вывод справочной информации о командах."""
-    print("\n*** Операции с данными ***")
+    print("\n*** Управление таблицами ***")
     print("Функции:")
-    print("<command> insert into <имя> values (<v1>, <v2>, ...) - создать запись")
-    print(
-        "<command> select from <имя> where <колонка> = <значение> "
-        "- выбрать по условию"
-    )
-    print("<command> select from <имя> - выбрать все записи")
-    print("<command> update <имя> set <k1>=<v1>[, ...] where <k>=<v> - обновить записи")
-    print("<command> delete from <имя> where <k>=<v> - удалить записи")
+    print("<command> create_table <имя> <столбец1:тип> ... - создать таблицу")
+    print("<command> list_tables - показать список всех таблиц")
+    print("<command> drop_table <имя> - удалить таблицу")
+
+    print("\n*** Операции с данными (CRUD) ***")
+    print("Функции:")
+    print("<command> insert into <имя> values (<v1>, <v2>, ...)")
+    print("  - добавить запись в таблицу")
+    print("<command> select from <имя> [where <столбец>=<значение>]")
+    print("  - выбрать записи")
+    print("<command> update <имя> set <столбец>=<значение>")
+    print("  where <столбец>=<значение> - обновить записи")
+    print("<command> delete from <имя> where <столбец>=<значение>")
+    print("  - удалить запись")
     print("<command> info <имя> - информация о таблице")
-    print("\nОбщие команды:")
+
+    print("\n*** Общие команды ***")
     print("<command> exit - выйти из программы")
     print("<command> help - справочная информация\n")
 
